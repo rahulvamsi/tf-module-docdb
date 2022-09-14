b3b333bdae5bae4d7702b5d8f7b2ba2eee56f493
Raghu K <raghuk.vit@gmail.com>
2022-09-14 08:21:49 +0530

    Update tf-module-docdb/

--- main.tf
+++ main.tf
@@ -1,17 +1,44 @@
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier  = "${var.env}-docdb"
-  engine              = "docdb"
-  engine_version      = var.engine_version
-  master_username     = local.username
-  master_password     = local.password
-  skip_final_snapshot = true
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier              = "${var.env}-docdb"
+  engine                          = "docdb"
+  engine_version                  = var.engine_version
+  master_username                 = local.username
+  master_password                 = local.password
+  skip_final_snapshot             = true
+  db_subnet_group_name            = aws_docdb_subnet_group.main.name
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+  vpc_security_group_ids          = [aws_security_group.main.id]
 }
 
-resource "aws_docdb_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+resource "aws_docdb_subnet_group" "main" {
+  name       = "${var.env}-docdb"
+  subnet_ids = var.apps_subnets_ids
 
   tags = {
-    Name = "My docdb subnet group"
+    Name = "${var.env}-docdb"
   }
 }
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb4.0"
+  name        = "${var.env}-docdb"
+  description = "${var.env}-docdb"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${var.env}-docdb"
+  description = "${var.env}-docdb"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "DOCUMENTDB"
+    from_port   = 27107
+    to_port     = 27107
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+  tags = {
+    Name = "${var.env}-docdb"
+  }
+}
+
--- vars.tf
+++ vars.tf
@@ -1,2 +1,5 @@
 variable "env" {}
 variable "engine_version" {}
+variable "apps_subnets_ids" {}
+variable "vpc_id" {}
+variable "vpc_cidr_block" {}
