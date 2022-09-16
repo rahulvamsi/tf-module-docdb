316ecfa2efdd1f0fabb08b818f792a36dc042365
Raghu K <raghuk.vit@gmail.com>
2022-09-16 07:57:59 +0530

    Update tf-module-docdb/

--- main.tf
+++ main.tf
@@ -49,3 +49,17 @@ resource "aws_security_group" "main" {
   }
 }
 
+resource "null_resource" "mongodb-schema-apply" {
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+unzip -o mongodb.zip
+cd mongodb-main
+curl -L -O https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${local.username} --password ${local.password} <catalogue.js 
+mongo --ssl --host ${aws_docdb_cluster.main.endpoint}:27017 --sslCAFile rds-combined-ca-bundle.pem --username ${local.username} --password ${local.password} <users.js
+EOF
+  }
+}
+
