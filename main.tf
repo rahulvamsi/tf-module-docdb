75f8841caf34a420c7649d8991560dad8ad00e3d
Raghu K <raghuk.vit@gmail.com>
2022-09-22 08:11:06 +0530

    Update terraform-modules/tf-module-docdb/

--- main.tf
+++ main.tf
@@ -65,13 +65,13 @@ EOF
 }
 
 resource "aws_ssm_parameter" "docdb-url-catalogue" {
-  name  = "mutable.docdb.catalogue.{{ENV}}.MONGO_URL"
+  name  = "mutable.docdb.catalogue.${var.env}.MONGO_URL"
   type  = "String"
   value = "mongodb://${local.username}:${local.password}@${aws_docdb_cluster.main.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
 
 resource "aws_ssm_parameter" "docdb-url-users" {
-  name  = "mutable.docdb.user.{{ENV}}.MONGO_URL"
+  name  = "mutable.docdb.user.${var.env}.MONGO_URL"
   type  = "String"
   value = "mongodb://${local.username}:${local.password}@${aws_docdb_cluster.main.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
 }
